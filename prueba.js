369354ca84546eae48d08372a016d7e2d44032ad
Carla <red_escarlata@RedEscarlata-Mac.home>
2019-10-20 20:43:54 +0200

    subo examen

---
+++ prueba.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/*let opcion, n1, n2;
+n1 = parseFloat(prompt('Ingrese su primer numero:'));
+n2 = (prompt('Ingrese su segundo numero:'));
+opcion = parseInt(prompt('Menu\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Potencia'));
+
+if (opcion == 1) {
+    console.log('\n' + (n1 + n2));
+
+} else if (opcion == 2) {
+    console.log('\n' + (n1 - n2));
+
+} else if (opcion == 3) {
+    console.log('\n' + (n1 * n2));
+
+} else if (opcion == 4) {
+    console.log('\n' + (n1 / n2));
+
+} else if (opcion == 5) {
+    console.log('\n' + Math.pow(n1, n2));
+
+} else {
+    console.log('la opción introducida no es valida');
+}
+
+*/
+//********************************************
+/*
+let totalScore = 0;
+const ENDGAME = 50;
+let roll;
+
+while (totalScore <= ENDGAME) {
+    roll = Math.floor((Math.random() * 6) + 1);
+    console.log(roll + ',' + totalScore);
+    totalScore = totalScore + roll;
+}
+
+console.log(roll + ',' + totalScore);
+*/
+
+//********+*************************************
+
+
+/*class Person {
+    name;
+    age;
+    gender;
+
+    constructor(names, ages, genders) {
+        this.name = names;
+        this.age = ages;
+        this.gender = genders;
+    }
+
+    mostrarDatos() {
+        console.log('hola me llamo ' + this.name + ' tengo ' + this.age + ' años y soy ' + this.gender);
+    }
+}
+
+class Student extends Person {
+    list;
+    subject;
+    group;
+    constructor(subject, group) {
+        this.subject = subject;
+        this.group = group;
+        this.list = [];
+    }
+    registrarLista(students) {
+        list.push (students);
+}
+
+}
+*/
+//No he podido completar el ejercicio número tres porque no llego a entender la forma para resolverlo. Mi nivel a día de hoy, creo que es "medio/bajo", os lo digo para orientaros cara al proyecto :(
+
+
+
+
+
+
+
+
